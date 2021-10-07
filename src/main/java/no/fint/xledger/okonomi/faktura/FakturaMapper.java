6f2231c26280b3cfce70c2bb5bcfc1b81b130f01
Trond Magnus Sevre <trond@fintlabs.no>
2021-10-07 11:56:03 +0200

    FB-16 #comment Faktura.Fakturert = use same logic as in fakturagrunnlag

--- src/main/java/no/fint/xledger/okonomi/faktura/FakturaMapper.java
+++ src/main/java/no/fint/xledger/okonomi/faktura/FakturaMapper.java
@@ -6,6 +6,7 @@ import no.fint.model.resource.Link;
 import no.fint.model.resource.okonomi.faktura.FakturaResource;
 import no.fint.model.resource.okonomi.faktura.FakturagrunnlagResource;
 import no.fint.xledger.model.salesOrders.Node;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.springframework.stereotype.Service;
 
@@ -26,8 +27,7 @@ public class FakturaMapper {
         Identifikator identifikator = new Identifikator();
         identifikator.setIdentifikatorverdi(salesOrder.getInvoiceNumber());
         faktura.setFakturanummer(identifikator);
-
-        faktura.setFakturert(faktura.getDato() != null);
+        faktura.setFakturert(isInvoiced(salesOrder));
         faktura.setForfallsdato(parseDate(salesOrder.getDueDate()));
         if (salesOrder.getCustomer() != null) faktura.setMottaker(salesOrder.getCustomer().getDescription());
         faktura.setRestbelop((long) salesOrder.getRemainingAmount() * 100L);
@@ -38,6 +38,10 @@ public class FakturaMapper {
         faktura.setBetalt(salesOrder.getRemainingAmount() <= 0);
 
         return faktura;
+    }
+
+    private boolean isInvoiced(Node salesOrder){
+        return StringUtils.isNoneEmpty(salesOrder.getInvoiceNumber()) && !salesOrder.getInvoiceNumber().equals("0");
     }
 
     private long toOre(String input) {
